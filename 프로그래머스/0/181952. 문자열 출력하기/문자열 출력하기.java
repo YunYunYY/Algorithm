cb18e288331516f06182e43d2c0560d888be6c9b
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2025-05-07 04:49:59 +0900

    [level 0] Title: 문자열 출력하기, Time: 143.48 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(bf.readLine());
+    }
+}
