d6e23567680e5b3417ce11b5e7d683a414c52149
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2025-10-12 21:35:40 +0900

    [Bronze V] Title: Correct, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stt = new StringTokenizer(bf.readLine());
+		int a = Integer.parseInt(stt.nextToken())-9;
+		int b = Integer.parseInt(stt.nextToken());
+		System.out.println(a*60+b);
+	}
+	
+}
