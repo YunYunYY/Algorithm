c15043a10f51f1c3d595d849d7c3c182e522c73e
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2025-10-12 21:26:04 +0900

    [Bronze V] Title: 사파리월드, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stt = new StringTokenizer(bf.readLine());
+		System.out.println(Math.abs(Long.parseLong(stt.nextToken())-Long.parseLong(stt.nextToken())));
+	}
+	
+}
