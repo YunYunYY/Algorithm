6678dca53cdada0be4be376ec2776cca3f8b8bb2
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2025-10-12 21:29:10 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(bf.readLine());
+		System.out.println(a*(a+1)/2);
+	}
+	
+}
