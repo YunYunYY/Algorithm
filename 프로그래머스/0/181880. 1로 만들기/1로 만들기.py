9408c5f42bffaa94905ba44f7376fc70905fa90f
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2026-02-26 22:54:33 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while 1:
+            if i==1:
+                break
+            if i%2==0:
+                i//=2
+            else:
+                i=(i-1)//2
+            answer+=1
+    return answer
